68713efb245f0e8059e2d7f452ce5bc3129d2045
Sebastien Le Maguer <slemaguer@coli.uni-saarland.de>
2016-10-16 13:27:07 +0200

    Reformat

--- src/main/java/de/dfki/mary/coefficientextraction/extraction/ExtractMGC.java
+++ src/main/java/de/dfki/mary/coefficientextraction/extraction/ExtractMGC.java
@@ -14,172 +14,176 @@ import java.util.Hashtable;
  */
 public class ExtractMGC extends ExtractBase
 {
-        private float samplerate;
-        private float freqwarp;
-        private float gamma;
-        private short order;
-        private boolean lngain_flag;
-        private boolean spectrum_flag;
+    private float samplerate;
+    private float freqwarp;
+    private float gamma;
+    private short order;
+    private boolean lngain_flag;
+    private boolean spectrum_flag;
 
-        public ExtractMGC()
+    public ExtractMGC()
+    {
+        setSampleRatekHz(48f);
+        setOrder((short) 49);
+        setGamma(0);
+        setLogGainFlag(true);
+        spectrum_flag = true;
+    }
+
+    public ExtractMGC(boolean spectrum_flag)
+    {
+        setSampleRatekHz(48f);
+        setOrder((short) 49);
+        setGamma(0);
+        setLogGainFlag(true);
+        spectrum_flag = false;
+    }
+
+    private void setFreqWarp(float sampleratekHz)
+    {
+
+        if (sampleratekHz == 8)
         {
-                setSampleRatekHz(48f);
-                setOrder((short) 49);
-                setGamma(0);
-                setLogGainFlag(true);
-                spectrum_flag = true;
+            freqwarp = 0.31f;
+        }
+        else if (sampleratekHz == 10)
+        {
+            freqwarp = 0.35f;
+        }
+        else if (sampleratekHz == 12)
+        {
+            freqwarp = 0.37f;
+        }
+        else if (sampleratekHz == 16)
+        {
+            freqwarp = 0.42f;
+        }
+        else if (sampleratekHz == 22.5)
+        {
+            freqwarp = 0.45f;
+        }
+        else if (sampleratekHz == 32)
+        {
+            freqwarp = 0.45f;
+        }
+        else if (sampleratekHz == 44.1)
+        {
+            freqwarp = 0.53f;
+        }
+        else if (sampleratekHz == 48)
+        {
+            freqwarp = 0.55f;
+        }
+        else
+        {
+            freqwarp = 0f; // FIXME: exception instead ?
+        }
+    }
+
+    public void setGamma(float gamma)
+    {
+        this.gamma = gamma;
+    }
+
+    public void setOrder(short order)
+    {
+        this.order = order;
+    }
+
+    public void setLogGainFlag(boolean lngain_flag)
+    {
+        this.lngain_flag = lngain_flag;
+    }
+
+    public void setLength(short length)
+    {
+        this.order = (short) (length - 1);
+    }
+
+    public void setSampleRate(float samplerate)
+    {
+        setSampleRatekHz((float)(samplerate * 0.001));
+    }
+
+    public void setSampleRatekHz(float samplerate)
+    {
+        this.samplerate = samplerate;
+        setFreqWarp(this.samplerate);
+    }
+
+    /**
+     *
+     *
+     */
+    public void extractFromSpectrum(String input_file_name, String output_file_name) throws Exception
+    {
+        Process p;
+        String error = "0"; // "1.0E-08"; // FIXME: should be included as an option or something like that
+
+        // 1. Generate full command
+        String command = "x2x +df " + input_file_name + " |";
+        if (gamma == 0)
+        {
+            command += 	"mcep -a " + freqwarp + " -m " + order + " -l 2048 -e " + error + " -j 0 -f 0.0 -q 3 > " + output_file_name;
+        }
+        else
+        {
+            String logGainOpt = "";
+            if (lngain_flag)
+            {
+                logGainOpt = " -l";
+            }
+            command += 	"mcep -a " + freqwarp + " -m " + order + " -l 2048 -e " + error + " -j 0 -f 0.0 -q 3 -o 4 | ";
+            command +=  "lpc2lsp -m " + order + logGainOpt + " -n 2048 -d " + error + " -p 8 > " + output_file_name;
         }
 
-        public ExtractMGC(boolean spectrum_flag)
+        // 2. extraction
+        String[] cmd = {"bash", "-c", command};
+        p = Runtime.getRuntime().exec(cmd);
+        p.waitFor();
+
+
+        BufferedReader reader =
+            new BufferedReader(new InputStreamReader(p.getInputStream()));
+
+        String line = "";
+        // while ((line = reader.readLine())!= null) {
+        //         System.out.println(line);
+        // }
+
+        StringBuilder sb = new StringBuilder();
+        reader =
+            new BufferedReader(new InputStreamReader(p.getErrorStream()));
+
+        line = "";
+        while ((line = reader.readLine())!= null) {
+            sb.append(line + "\n");
+        }
+        if (!sb.toString().isEmpty())
         {
-                setSampleRatekHz(48f);
-                setOrder((short) 49);
-                setGamma(0);
-                setLogGainFlag(true);
-                spectrum_flag = false;
+            throw new Exception(sb.toString());
         }
 
-        private void setFreqWarp(float sampleratekHz)
+    }
+
+
+    public void extractFromWav(String input_file_name, String output_file_name) throws Exception
+    {
+        throw new Exception("Method extractFromWav not implemented yet");
+    }
+
+    public void extract(String input_file_name) throws Exception
+    {
+
+        String[] tokens = (new File(input_file_name)).getName().split("\\.(?=[^\\.]+$)");
+        String output_file_name = extToDir.get("mgc") + "/" + tokens[0] + ".mgc";
+        if (spectrum_flag)
         {
-
-                if (sampleratekHz == 8)
-                {
-                        freqwarp = 0.31f;
-                }
-                else if (sampleratekHz == 10)
-                {
-                        freqwarp = 0.35f;
-                }
-                else if (sampleratekHz == 12)
-                {
-                        freqwarp = 0.37f;
-                }
-                else if (sampleratekHz == 16)
-                {
-                        freqwarp = 0.42f;
-                }
-                else if (sampleratekHz == 22.5)
-                {
-                        freqwarp = 0.45f;
-                }
-                else if (sampleratekHz == 32)
-                {
-                        freqwarp = 0.45f;
-                }
-                else if (sampleratekHz == 44.1)
-                {
-                        freqwarp = 0.53f;
-                }
-                else if (sampleratekHz == 48)
-                {
-                        freqwarp = 0.55f;
-                }
-                else
-                {
-                        freqwarp = 0f; // FIXME: exception instead ?
-                }
+            extractFromSpectrum(input_file_name, output_file_name);
         }
-
-        public void setGamma(float gamma)
+        else
         {
-                this.gamma = gamma;
+            extractFromWav(input_file_name, output_file_name);
         }
-
-        public void setOrder(short order)
-        {
-                this.order = order;
-        }
-
-        public void setLogGainFlag(boolean lngain_flag)
-        {
-                this.lngain_flag = lngain_flag;
-        }
-
-        public void setLength(short length)
-        {
-                this.order = (short) (length - 1);
-        }
-
-        public void setSampleRate(float samplerate)
-        {
-                setSampleRatekHz((float)(samplerate * 0.001));
-        }
-
-        public void setSampleRatekHz(float samplerate)
-        {
-                this.samplerate = samplerate;
-                setFreqWarp(this.samplerate);
-        }
-
-        public void extractFromSpectrum(String input_file_name, String output_file_name) throws Exception
-        {
-                Process p;
-                String error = "0"; // "1.0E-08"; // FIXME: should be included as an option or something like that
-
-                // 1. Generate full command
-                String command = "x2x +df " + input_file_name + " |";
-                if (gamma == 0)
-                {
-                        command += 	"mcep -a " + freqwarp + " -m " + order + " -l 2048 -e " + error + " -j 0 -f 0.0 -q 3 > " + output_file_name;
-                }
-                else
-                {
-                        String logGainOpt = "";
-                        if (lngain_flag)
-                        {
-                                logGainOpt = " -l";
-                        }
-                        command += 	"mcep -a " + freqwarp + " -m " + order + " -l 2048 -e " + error + " -j 0 -f 0.0 -q 3 -o 4 | ";
-                        command +=  "lpc2lsp -m " + order + logGainOpt + " -n 2048 -d " + error + " -p 8 > " + output_file_name;
-                }
-
-                // 2. extraction
-                String[] cmd = {"bash", "-c", command};
-                p = Runtime.getRuntime().exec(cmd);
-                p.waitFor();
-
-
-                BufferedReader reader =
-                        new BufferedReader(new InputStreamReader(p.getInputStream()));
-
-                String line = "";
-                // while ((line = reader.readLine())!= null) {
-                //         System.out.println(line);
-                // }
-
-                StringBuilder sb = new StringBuilder();
-                reader =
-                        new BufferedReader(new InputStreamReader(p.getErrorStream()));
-
-                line = "";
-                while ((line = reader.readLine())!= null) {
-                        sb.append(line + "\n");
-                }
-                if (!sb.toString().isEmpty())
-                {
-                        throw new Exception(sb.toString());
-                }
-
-        }
-
-
-        public void extractFromWav(String input_file_name, String output_file_name) throws Exception
-        {
-                throw new Exception("Method extractFromWav not implemented yet");
-        }
-
-        public void extract(String input_file_name) throws Exception
-        {
-
-                String[] tokens = (new File(input_file_name)).getName().split("\\.(?=[^\\.]+$)");
-                String output_file_name = extToDir.get("mgc") + "/" + tokens[0] + ".mgc";
-                if (spectrum_flag)
-                {
-                        extractFromSpectrum(input_file_name, output_file_name);
-                }
-                else
-                {
-                        extractFromWav(input_file_name, output_file_name);
-                }
-        }
+    }
 }
