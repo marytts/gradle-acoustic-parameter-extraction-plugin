2d8e441a3239f634ea920a0b0762dca019e4d7b7
Sebastien Le Maguer <slemaguer@coli.uni-saarland.de>
2016-10-16 13:46:05 +0200

    Throwing exception

--- src/main/java/de/dfki/mary/coefficientextraction/extraction/ExtractWorld.java
+++ src/main/java/de/dfki/mary/coefficientextraction/extraction/ExtractWorld.java
@@ -64,7 +64,7 @@ public class ExtractWorld extends ExtractBase
         }
     }
 
-    private void f0Conversion(String input_file_name, String output_file_name)
+    private void f0Conversion(String input_file_name, String output_file_name) throws Exception
     {
         Process p;
 
@@ -96,7 +96,7 @@ public class ExtractWorld extends ExtractBase
         }
     }
 
-    private void spectrumConversion(String input_file_name, String output_file_name)
+    private void spectrumConversion(String input_file_name, String output_file_name) throws Exception
     {
         Process p;
 
